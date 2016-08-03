6b8823c56d986372fa778135f4bdcafdcf6b7762
Marc Humet <marc.humet@gmail.com>
2016-08-03 14:05:46 +0200

    Documenting what Haneke does behind the scenes with images

--- Marvel Heroes/Views/HeroCell.swift
+++ Marvel Heroes/Views/HeroCell.swift
@@ -29,7 +29,33 @@ class HeroCell: UICollectionViewCell {
         
         
          guard let url = NSURL.init(string: self.hero.thumbnailUrl) else { print("Not a valid url"); return}
+        
+        
+        /*Stack trace for retrieval of image:
+         (UIMageView+Haneke.swift)
+         UIImageView.hnk_setImageFromURL -> 
+            UIImageView.hnk_setImageFromFetcher -> 
+                UIImageView.hnk_fetchImageForFetcher ->
+                 let cache = Shared.imageCache (we acces the singleton that holds the shared cache)
+                    (Haneke.swift) 
+                        static Shared.imageCache.getter
+         */
             thumbImg.hnk_setImageFromURL(url)
+        
+        /*
+         https://github.com/Haneke/HanekeSwift
+         
+         The above line take care of:
+         
+         If cached, retrieving an appropriately sized image (based on the bounds and contentMode of the UIImageView) from the memory or disk cache. Disk access is performed in background.
+         If not cached, loading the original image from web/memory and producing an appropriately sized image, both in background. Remote images will be retrieved from the shared NSURLCache if available.
+         Setting the image and animating the change if appropriate.
+         Or doing nothing if the UIImageView was reused during any of the above steps.
+         Caching the resulting image.
+         If needed, evicting the least recently used images in the cache.
+         
+         */
+        
             thumbImg.layer.borderWidth = 1
             thumbImg.layer.masksToBounds = false
             thumbImg.layer.borderColor = UIColor.whiteColor().CGColor
