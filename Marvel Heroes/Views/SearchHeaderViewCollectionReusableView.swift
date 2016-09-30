759a68e0c2e8ccddc5e1270e293c9a2026881a94
Marc Humet <marc.humet@gmail.com>
2016-09-30 11:50:48 +0200

    Updating project references. Adding a search Header View that will hold the searchbar in the top.

---
+++ Marvel Heroes/Views/SearchHeaderViewCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  SearchHeaderViewCollectionReusableView.swift
+//  Marvel Heroes
+//
+//  Created by Marc Humet Sors on 9/21/16.
+//  Copyright © 2016 SPM. All rights reserved.
+//
+
+import UIKit
+
+class SearchHeaderViewCollectionReusableView: UICollectionReusableView {
+        
+}
